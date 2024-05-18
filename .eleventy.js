5d5a30d71ba413f407023f9edc6722b8b58f13c7
miguel.amor <miguel.amor@gmail.com>
2024-05-18 19:45:14 +0200

    2nd modify pathprefix for GitHub pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("code/css");
   eleventyConfig.addPassthroughCopy("code/js");
   eleventyConfig.addPassthroughCopy("code/img");
+
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
   return {
     dir: {
